34dfc1b02b384c07da9bef292247c60ff79a492d
Jherich Silas <jjsilas@gmail.com>
2020-09-13 23:27:52 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/rfslogopass.png';
 import './App.css';
+
 
 function App() {
   return (
@@ -8,16 +9,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The new version of Royalty Free Studio
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Version 2
+        </p>
       </header>
     </div>
   );
